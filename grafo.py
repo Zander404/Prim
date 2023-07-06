a54e21109e76cdaa69e72c452fc47ee6265a3e10
zander404 <alexandrearthurpg@gmail.com>
2023-07-05 21:09:04 -0300

    fazendo avanços

--- grafo.py
+++ grafo.py
@@ -2,18 +2,26 @@ import random
 import networkx as nx
 
 
-def generate_random_edgelist(num_nodes, num_edges, weight_range=(1, 10)):
-    if num_nodes <= 0:
-        raise ValueError("Number of nodes must be greater than 0.")
+def generate_random_graph(num_nodes, num_edges, weight_range=(1, 5)):
+    if num_nodes <= 0 or num_edges <= 0:
+        raise ValueError("Number of nodes and edges must be greater than 0.")
 
-    edges = []
-    nodes = set(range(1, num_nodes + 1))
+    # Create an empty graph
+    g = nx.Graph()
 
-    while len(edges) < num_edges and len(nodes) > 1:
-        u = random.choice(list(nodes))
-        nodes.remove(u)
-        v = random.choice(list(nodes))
+    # Add nodes to the graph
+    g.add_nodes_from(range(1, num_nodes + 1))
+
+    # Add random edges to the graph
+    for _ in range(num_edges):
+        u = random.randint(1, num_nodes)
+        v = random.randint(1, num_nodes)
+        while u == v or g.has_edge(u, v):
+            u = random.randint(1, num_nodes)
+            v = random.randint(1, num_nodes)
         weight = random.randint(weight_range[0], weight_range[1])
-        edges.append((u, v, weight))
+        g.add_edge(u, v, weight=weight)
 
-    return edges
+    return g
+
+
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import random
 import networkx as nx
 import prim as mst
 import matplotlib.pyplot as plt
-from grafo import generate_random_edgelist
+from grafo import generate_random_graph
+import numpy as np
+
 """
 Gerar o grafo com seus pesos
 """
@@ -11,25 +13,28 @@ Gerar o grafo com seus pesos
 nodes = int(input("Quantos vertices vc deseja? "))
 edge = int(input("Quantos arestas vc deseja? "))
 
-random_list = generate_random_edgelist(nodes, edge, weight_range=(1, 10))
+random_list = generate_random_graph(nodes, edge, weight_range=(1, 10))
 
+edge_list = list(random_list.edges(data=True))
 filename = 'sukita.txt'
 
 with open(filename, 'w') as file:
-    for u, v, weight in random_list:
+    for u, v, data in edge_list:
+        weight = data['weight']
         file.write(f"{u} {v} {weight}\n")
+
 
 g = nx.read_edgelist(filename, create_using=nx.Graph(), nodetype=int, data=(('weight', int),))
 sp = nx.spring_layout(g)
 
+adj_matrix = nx.adjacency_matrix(g).todense()
+
+for row in adj_matrix:
+    print(" ".join(map(str, row)).replace("0", " "), end="\n")
+
+
 arestas = nx.get_edge_attributes(g, 'weight')
-
 root = random.choice(list(g.nodes))
-
-plt.axis('on')
-nx.draw_networkx(g, sp, with_labels=True)
-nx.draw_networkx_edge_labels(g, sp, edge_labels=arestas)
-plt.show()
 
 """
 Gerar Arvore MST
